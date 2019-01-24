a12d0788f8a8166fcf89c64a6e8700f5a15b4ca1
Cheng Vue <cvu3@ucsc.edu>
2019-01-24 05:05:52 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -16,8 +16,8 @@ class ApplicationController < ActionController::Base
 
   def create
     @coupon = Coupon.new
-    @coupon.coupon_code = params[:coupon_code] 
-    @coupon.store = params[:store] 
+    @coupon.coupon_code = params[:coupon_code]
+    @coupon.store = params[:store]
     @coupon.save
 
     redirect_to coupon_path(@coupon)
