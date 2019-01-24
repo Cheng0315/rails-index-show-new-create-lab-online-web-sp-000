11b91f60658be153759f4cc7787661bdc5c6c024
Cheng Vue <cvu3@ucsc.edu>
2019-01-24 05:06:43 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,24 +2,4 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
-
-  def index
-    @coupons = Coupon.all
-  end
-
-  def show
-    @student = Student.find(params[:id])
-  end
-
-  def new
-  end
-
-  def create
-    @coupon = Coupon.new
-    @coupon.coupon_code = params[:coupon_code]
-    @coupon.store = params[:store]
-    @coupon.save
-
-    redirect_to coupon_path(@coupon)
-  end
 end
